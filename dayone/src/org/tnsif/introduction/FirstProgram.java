1680fec17369377c661c4c01c87fc16809bc466d
dishanttare21~ <taredishant21@gmail.com>
2023-04-03 16:11:58 +0530

    First Commit

---
+++ dayone/src/org/tnsif/introduction/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.tnsif.introduction;
+
+//Program to print hello world
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
