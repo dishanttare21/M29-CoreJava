89bef61db1e99d106e07199438e389a9eb2654d5
dishanttare21~ <taredishant21@gmail.com>
2023-04-04 17:59:49 +0530

    Second Commit

---
+++ dayone/src/org/tnsif/introduction/CharDemo.java
@@ -0,0 +1,19 @@
+package org.tnsif.introduction;
+
+public class CharDemo {
+	public static void main(String[] args )
+	{
+		//	Program to demonstrate on character
+		char ch='A';
+		System.out.println(ch);
+		
+		//	assigning number to character
+		char ch1=71;
+		System.out.println(ch1);
+		
+		// assigning unicode to character
+		char ch2='\u00A3';
+		System.out.println(ch2);
+}
+
+}
---
+++ dayone/src/org/tnsif/introduction/SecondProgram.java
@@ -0,0 +1,22 @@
+package org.tnsif.introduction;
+
+//Programs to demonstrate on various data types
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// int gives us the whole number
+		// float gives the decimal number
+
+		int num1=5/2; 	
+		//if we use f after any number float gives precision upto 7 decimal number
+		float num2=103f/44f;	
+		//if we use d after any number double gives the precision upto 16 decimal places
+		double num3=10d/3d;
+		System.out.println("V1: "+num1);
+		System.out.println("V2: "+num2);
+		System.out.println("V3: "+num3);
+		
+	}
+
+}
