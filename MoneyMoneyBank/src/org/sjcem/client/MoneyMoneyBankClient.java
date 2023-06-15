b496e800e6f43ca4fc489b457f99101718e42e8d
dishanttare21~ <taredishant21@gmail.com>
2023-06-15 20:06:01 +0530

    First commit

---
+++ MoneyMoneyBank/src/org/sjcem/client/MoneyMoneyBankClient.java
@@ -0,0 +1,35 @@
+//Driver class
+//Program to demonstrate on MoneyMoneyBank App
+package org.sjcem.client;
+
+import org.sjcem.application.MMBankFactory;
+import org.sjcem.application.MMCurrentAcc;
+import org.sjcem.application.MMSavingAcc;
+import org.sjcem.framework.BankFactory;
+import org.sjcem.framework.CurrentAcc;
+import org.sjcem.framework.SavingAcc;
+
+public class MoneyMoneyBankClient {
+
+	public static void main(String[] args) {
+		BankFactory b = new MMBankFactory();
+		SavingAcc s = new MMSavingAcc(1111, "Dishant Tare", 3000, true);
+		CurrentAcc c = new MMCurrentAcc(2222, "Abhinav Rawat", 5000, 4500);
+		
+		System.out.println(s);
+		System.out.println(c);
+		
+		System.out.println();
+		
+		System.out.println("Saving Account");
+		
+		s.withdraw(200);
+		
+		
+		System.out.println("\nCurrent Account");
+		c.withdraw(400);
+		
+		
+	}
+
+}
